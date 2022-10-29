54e33466062e99a0192a3795d230b35e61e1197b
danh2411 <72093554+danh2411@users.noreply.github.com>
2022-10-29 13:33:03 +0700

    data-jwt

---
+++ database/migrations/2022_10_29_043650_bills.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Bills extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('account_id');
+            $table->unsignedBigInteger('client_id');
+            $table->integer('received_date');
+            $table->integer('payday');
+            $table->integer('total_room_rate');
+            $table->integer('total_service_fee');
+            $table->integer('total_money');
+
+
+            $table->foreign('account_id')
+                ->references('id')->on('clients')
+                ->onDelete('cascade');
+            $table->foreign('client_id')
+                ->references('id')->on('accounts')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
