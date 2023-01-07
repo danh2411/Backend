91f0b2d98ea04fa2dc787077259b711a73afaa7d
danh2411 <72093554+danh2411@users.noreply.github.com>
2023-01-07 11:10:01 +0700

    fixapi

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -760,7 +760,7 @@ class BillController extends Controller
             $bi['lastname'] = $client->lastname;
             $bi['phone'] = $client->phone;
             $bi['CCCD'] = $client->CCCD;
-            $bi['service'] = $list??null;
+            $bi['service'] = $list;
             $bi['total_room_rate '] = $bill-> total_room_rate ;
             $bi['total_service_fee '] = $bill-> total_service_fee ;
             $bi['total_money '] = $bill-> total_money ;
